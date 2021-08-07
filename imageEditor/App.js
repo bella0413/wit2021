9d817b8083c4cfd891643cad4809d4abf914c5eb
YoonJeon <yoonjeon0413@gmail.com>
2021-08-07 17:45:20 +0900

    Initial commit of imageEditor

---
+++ imageEditor/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>imageEditor</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default App;
