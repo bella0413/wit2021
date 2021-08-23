bbc539e4fa74f43b35170f9159b384f00865a07d
YoonJeon <yoonjeon0413@gmail.com>
2021-08-23 17:12:02 +0900

    Add more option at ImagePicker

--- imageEditor/screens/ImageEditScreen.js
+++ imageEditor/screens/ImageEditScreen.js
@@ -1,10 +1,9 @@
 import React, {useState} from 'react';
 import {
-  View,
-  Text,
-  ImageBackground,
+    View,
+    Text,
+    ImageBackground,
     StyleSheet,
-    Button,
     TouchableOpacity,
 } from 'react-native';
 
@@ -16,33 +15,52 @@ import Animated from 'react-native-reanimated';
 import ImagePicker from 'react-native-image-crop-picker';
 
 const ImageEditScreen = () => {
-
     const [image, setImage] = useState('https://images.unsplash.com/photo-1576515652031-fc429bab6503?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80');
                         
     const takePhotoFromCamera = () => {
-        ImagePicker.openCamera({
-            compressImageMaxWidth: 300,
-            compressImageMaxHeight: 300,
-            cropping: true,
-            compressImageQuality: 0.7
-        }).then(image => {
-            console.log(image);
-            setImage(image.path);
-            bs.current.snapTo(1);
-        });
+        try {
+            ImagePicker.openCamera({
+                compressImageMaxWidth: 300,
+                compressImageMaxHeight: 300,
+                cropperActiveWidgetColor: 'blue',
+                cropperToolbarColor: 'yellow',
+                cropperStatusBarColor: 'red',
+                cropperToolbarWidgetColor: 'green',
+                cropperToolbarTitle: '티끌',
+                cropping: true,
+                compressImageQuality: 0.7
+            }).then(image => {
+                console.log(image);
+                setImage(image.path);
+                bs.current.snapTo(1);
+            });
+        } catch (errpr) {
+            console.log(error);
+            return (false);
+        }
     }
-
+    
     const choosePhotoFromLibrary = () => {
-        ImagePicker.openPicker({
-            width: 300,
-            height: 300,
-            cropping: true,
-            compressImageQuality: 0.7
-        }).then(image => {
-            console.log(image);
-            setImage(image.path);
-            bs.current.snapTo(1);
-        });
+        try {
+            ImagePicker.openPicker({
+                width: 300,
+                height: 300,
+                cropperActiveWidgetColor: 'blue',
+                cropperStatusBarColor: 'red',
+                cropperToolbarColor: 'yellow',
+                cropperToolbarWidgetColor: 'green'
+                cropperToolbarTitle: '티끌',
+                cropping: true,
+                compressImageQuality: 0.7
+            }).then(image => {
+                console.log(image);
+                setImage(image.path);
+                bs.current.snapTo(1);
+            });
+        } catch (error) {
+            console.log(error);
+            return (false);
+        }
     }
 
     const renderContent = () => (
@@ -79,54 +97,6 @@ const ImageEditScreen = () => {
 
     return (
         <View style={styles.container}>
-            {/* <View style={styles.panel}>
-            <View style={{alignItems: 'center'}}>
-            <Text style={styles.panelTitle}>Upload Photo</Text>
-            <Text style={styles.panelSubtitle}>Choose Your Picture</Text>
-            </View>
-            <TouchableOpacity onPress={takePhotoFromCamera}>
-            <Text style={styles.panelButtonTitle}>Take Photo</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={choosePhotoFromLibrary}>
-            <Text style={styles.panelButtonTitle}>Choose From Library</Text>
-            </TouchableOpacity>
-            </View>
-            <View
-                style={{
-                height: 100,
-                width: 100,
-                borderRadius: 15,
-                justifyContent: 'center',
-                alignItems: 'center',
-                }}>
-                <ImageBackground
-                source={{
-                    uri: image,
-                }}
-                style={{height: 100, width: 100}}
-                imageStyle={{borderRadius: 15}}>
-                    <View
-                        style={{
-                        flex: 1,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        }}>
-                        <Icon
-                        name="camera"
-                        size={35}
-                        color="#fff"
-                        style={{
-                            opacity: 0.7,
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            borderWidth: 1,
-                            borderColor: '#fff',
-                            borderRadius: 10,
-                        }}
-                        />
-                    </View>
-                </ImageBackground>
-            </View> */}
             <BottomSheet
             ref={bs}
             snapPoints={[330, 0]}
@@ -134,52 +104,50 @@ const ImageEditScreen = () => {
             renderHeader={renderHeader}
             initialSnap={1}
             callbackNode={fall}
-                enabledGestureInteraction={true}
-                enabledContentTapInteraction={false}
-                    />
-        <Animated.View style={{margin: 20,
-        opacity: Animated.add(0.1, Animated.multiply(fall, 1.0)), flex:1, justifyContent: 'center',
-            }}>
-        <Text style={{fontSize:25, fontWeight: '700', paddingBottom: 80}}>Iamge Editor Pactice</Text>
-        <View style={{alignItems: 'center'}}>
-          <TouchableOpacity onPress={() => bs.current.snapTo(0)}>
-            <View
-              style={{
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <ImageBackground
-                source={{
-                  uri: image,
-                }}
-                style={{height: 200, width: 200}}
-                imageStyle={{borderRadius: 15}}>
-                <View
-                  style={{
-                    flex: 1,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                  }}>
-                  <Icon
-                    name="camera"
-                    size={35}
-                    color="#fff"
-                    style={{
-                      opacity: 0.7,
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                      borderWidth: 1,
-                      borderColor: '#fff',
-                      borderRadius: 10,
-                    }}
-                  />
+            enabledGestureInteraction={true}
+            enabledContentTapInteraction={false}/>
+            <Animated.View style={{margin: 20,
+            opacity: Animated.add(0.1, Animated.multiply(fall, 1.0)), flex:1, justifyContent: 'center',}}>
+                <Text style={{fontSize:25, fontWeight: '700', paddingBottom: 80}}>Iamge Editor Pactice</Text>
+                <View style={{alignItems: 'center'}}>
+                    <TouchableOpacity onPress={() => bs.current.snapTo(0)}>
+                        <View
+                        style={{
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}>
+                            <ImageBackground
+                                source={{
+                                uri: image,
+                                }}
+                                style={{height: 200, width: 200}}
+                                imageStyle={{borderRadius: 15}}>
+                                    <View
+                                    style={{
+                                        flex: 1,
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                    }}>
+                                    <Icon
+                                        name="camera"
+                                        size={35}
+                                        color="#fff"
+                                        style={{
+                                        opacity: 0.7,
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        borderWidth: 1,
+                                        borderColor: '#fff',
+                                        borderRadius: 10,
+                                        }}
+                                    />
+                                    </View>
+                            </ImageBackground>
+                        </View>
+                    </TouchableOpacity>
                 </View>
-              </ImageBackground>
-            </View>
-            </TouchableOpacity>
-            </View>
             </Animated.View>
-            </View>
+        </View>
     );
 };
     
